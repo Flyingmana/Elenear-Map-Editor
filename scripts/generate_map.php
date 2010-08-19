ae61b78fbee5b186bf13bd1929ca1971bb6e1141
flyingmana <flyingmana@googlemail.com>
2010-08-19 20:20:33 +0200

    add random map generator script

---
+++ scripts/generate_map.php
@@ -0,0 +1,24 @@
+<?php
+
+    $count_x = 25;
+    $count_y = 25;
+
+    $types = array(
+        1 => "Wiese",
+        2 => "Berg",
+        3 => "Wasser"
+    );
+
+    echo '{
+    "name": "flyingmana",
+    "map": [
+        ';
+    for($x = 1; $x<=$count_x; $x++){
+        for($y = 1; $y<=$count_y; $y++){
+            echo "\t\t{ \"x\":$x, \"y\":$y, \"typ\": ".rand(1,3)." },".PHP_EOL;
+        }
+    };
+    echo '    ]
+}'.PHP_EOL;
+
+?>
